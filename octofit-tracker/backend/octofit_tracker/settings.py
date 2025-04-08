bbf37438304e6a8b8650a2fc2d9eca5fe3b0342a
Kevin Backhouse <kevinbackhouse@github.com>
2025-04-08 22:17:59 +0000

    WIP

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-lauq70trww#hfjt^mc3@p@=b#5$h)ehfws^=z3p+=#1q(hf_wr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
